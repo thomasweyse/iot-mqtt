3a1fcb013f23b6e1c5d4171023c265917123c409
Thomas Kurt Weyse <143903482+thomasweyse@users.noreply.github.com>
2024-10-16 15:47:45 +0200

    added template for wifi connection

---
+++ src/smarthomeTemplate.h
@@ -0,0 +1,12 @@
+// change filename to smarthome.h and change settings
+
+// WiFi
+const char *SSID = "WifiSSID";
+const char *PASSWORD = "WifiPasswd";
+
+// MQTT Broker
+const char *MQTT_BROKER = "mqtt.domain.eu";
+const char *TOPIC = "TOPIC";
+const char *MQTT_USER = "USER";
+const char *MQTT_PWD = "PWD";
+const int MQTT_PORT = 1883;

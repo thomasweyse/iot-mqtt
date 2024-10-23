7275c6ffe9694d7ebfcd4c0a877668bd12fdc300
Thomas Kurt Weyse <143903482+thomasweyse@users.noreply.github.com>
2024-10-23 18:18:25 +0200

    prototyping zu mqtt-transfer

--- src/smarthome.cpp
+++ src/smarthome.cpp
@@ -10,7 +10,8 @@
 #include <WiFiClientSecure.h>
 
 // init wifi secure client
-WiFiClientSecure espClient;
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
 
 int progress = 0;
 char getProgress(int progress);
@@ -22,21 +23,53 @@ LiquidCrystal_I2C mylcd(0x27,16,2);
 xht11 xht(17);
 unsigned char dht[4] = {0, 0, 0, 0};
 
+void callback(char *topic, byte *payload, unsigned int length) {
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    for (int i = 0; i < length; i++) {
+        Serial.print((char) payload[i]);
+    }
+    Serial.println();
+    Serial.println("-----------------------");
+}
+
+char mqttDataStream[64];
+void generateMQTTData(unsigned char dht[4]);
 
 void setup() {
+    Serial.begin(115200);
+
     // Initialize LCD
     mylcd.init();
     mylcd.backlight();
 
-    // TODO think about certs / TKW / 18.10.2024
-    // espClient.setCACert(ca_cert);
-
     // Establish connection to IoT Broker
     WiFi.begin(SSID, PASSWORD);
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED) 
+    {
         delay(500);
         printStateLCD("Init WiFi/MQTT");
     }
+    printStateLCD("               ");
+
+    mqttClient.setServer(MQTT_BROKER, MQTT_PORT);
+    mqttClient.setCallback(callback);
+
+    while (!mqttClient.connected()) 
+    {
+        String client_id = "esp32-client-";
+        client_id += String(WiFi.macAddress());
+        Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
+        if (mqttClient.connect(client_id.c_str(), MQTT_USER, MQTT_PWD)) {
+            Serial.println("Public EMQX MQTT broker connected");
+        } else {
+            Serial.print("failed with state ");
+            Serial.print(mqttClient.state());
+            delay(2000);
+        }
+    }
+    Serial.print("-> setup() completed!");
 }
 
 void loop()
@@ -44,13 +77,24 @@ void loop()
     if (xht.receive(dht))
     {
         printTempLCD(progress);
-        progress++;
         
+        if(progress == 0)
+        {
+            //mqttClient.publish(MQTT_TOPIC, "Hallo erstmal!");
+            generateMQTTData(dht);
+            mqttClient.publish(MQTT_TOPIC, mqttDataStream);
+            mqttClient.subscribe(MQTT_TOPIC);
+        }
+
+        progress++;
+
         if(progress == 2)
         {
             progress = 0;
         }
     }
+
+    delay(3000);
 }
 
 char getProgress(int progress)
@@ -97,4 +141,10 @@ void printStateLCD(const String message){
 
 void clearStateLCD(){
     mylcd.clear();
+}
+
+void generateMQTTData(unsigned char dht[4]){
+    int temp = dht[2];
+    int humi = dht[0];
+    snprintf( mqttDataStream, sizeof(mqttDataStream), "%d %d" , temp, humi);
 }
